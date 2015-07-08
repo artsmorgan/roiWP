07cd9e8ee23e468f299eaa458c9b49fea0c6b2c5
Aletz Morgan <amorgan115@gmail.com>
2015-07-07 23:51:28 -0600

    add adminize to site

--- wp-content/themes/twentythirteen/functions.php
+++ wp-content/themes/twentythirteen/functions.php
@@ -564,3 +564,36 @@ function remove_admin_bar() {
 }
 add_action('after_setup_theme', 'remove_admin_bar');
 
+
+function remove_menus()
+{
+    // global $menu;
+    // global $current_user;
+    // get_currentuserinfo();
+    // print_r(wp_get_current_user()->roles);die();
+    $role = wp_get_current_user()->roles;
+    if($role == 'administrator')
+    {
+        $restricted = array(__('Pages'),
+                            __('Media'),
+                            __('Links'),
+                            __('Custom Post Name'),
+                            __('Comments'),
+                            __('Appearance'),
+                            __('Plugins'),
+                            __('Users'),
+                            __('Tools'),
+                            __('Settings'),
+                            __('Posts'),
+
+        );
+        end ($menu);
+        while (prev($menu)){
+            $value = explode(' ',$menu[key($menu)][0]);
+            if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){unset($menu[key($menu)]);}
+        }// end while
+
+    }// end if
+}
+add_action('admin_menu', 'remove_menus');
+
